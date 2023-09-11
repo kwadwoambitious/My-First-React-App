e8573dc7426dac2122167f82163ee9095d36c980
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-11 17:48:45 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-
-const App  = () => {
-  const name = "John!";
-  const isNameShowing = true;
-
-  return (
-    <div className="App">
-      <h1>Hello, {isNameShowing ? name : "Someone!"}</h1>
-    </div>
-  )
-}
-
-export default App;
-=======
 import React from 'react';
 import './App.css';
 import YoutubeChannel from './YoutubeChannel';
@@ -27,4 +11,3 @@ const App = () => {
 }
 
 export default App;
->>>>>>> 3e5ea34 (First Commit)
